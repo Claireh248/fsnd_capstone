16f832a8220dd26e7a6a780e69698edd969a7eca
Claire Higgins <claireeh2000@googlemail.com>
2024-03-22 15:22:26 +0000

    Update tokens & tests setup

--- setup_tests.sh
+++ setup_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo -u pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
 sudo -u postgres dropdb capstone_test
 sudo -u postgres createdb capstone_test
 sudo -u postgres psql capstone_test < capstone.psql
